485e35edea51cbe50d350520acb7de49be2c219b
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-10 16:41:17 +0200

    Add project files.

---
+++ NfiSharpLab6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NfiSharpLab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in ett tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mata in ett till tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(tal1+tal2);
+        }
+    }
+}
